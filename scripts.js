2b7242d61c853632e82a85b7a694b4668a9f833a
Thayna Mac Dolw <thaymacdolw@gmail.com>
2023-02-02 20:02:24 -0300

    Creating the JS code

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// VARIÁVEIS => Um espaço da memória do PC que guardamos algo. Um número, um texto, uma imagem...
+// Funçãõ => um trecho de código que só é executado quando é chamado.
+
+let abacate = 2
+console.log (abacate)
+console.log ("E aí galera?")
+
+function mario(){
+    console.log ("Estou dentro da função")
+}
+
+mario () // chamando a fução
+
+console.log ("E aí, deu o like?")
